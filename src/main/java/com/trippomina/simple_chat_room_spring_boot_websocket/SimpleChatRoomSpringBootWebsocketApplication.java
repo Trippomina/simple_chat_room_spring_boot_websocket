947cc075e49e8fce57a3bdc1ef6920729d96589a
Ahmad-H-Khouja <ahmad.h.khouja@gmail.com>
2023-03-02 07:48:07 +0200

    project backend initialized

---
+++ src/main/java/com/trippomina/simple_chat_room_spring_boot_websocket/SimpleChatRoomSpringBootWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.trippomina.simple_chat_room_spring_boot_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatRoomSpringBootWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatRoomSpringBootWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trippomina/simple_chat_room_spring_boot_websocket/SimpleChatRoomSpringBootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trippomina.simple_chat_room_spring_boot_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatRoomSpringBootWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
