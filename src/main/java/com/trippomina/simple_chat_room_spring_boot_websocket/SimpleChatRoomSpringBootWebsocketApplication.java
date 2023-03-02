7a285fe83b0363e8d179ed4fb0f5de7a09784f13
Ahmad-H-Khouja <ahmad.h.khouja@gmail.com>
2023-03-02 08:24:05 +0200

    test 1

--- src/main/java/com/trippomina/simple_chat_room_spring_boot_websocket/SimpleChatRoomSpringBootWebsocketApplication.java
+++ src/main/java/com/trippomina/simple_chat_room_spring_boot_websocket/SimpleChatRoomSpringBootWebsocketApplication.java
@@ -9,5 +9,5 @@ public class SimpleChatRoomSpringBootWebsocketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleChatRoomSpringBootWebsocketApplication.class, args);
 	}
-
+//
 }
